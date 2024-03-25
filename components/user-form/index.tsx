f986dd7993593808e622c04597d9589ff9436137
Ignacio Heredia <ignacio.nh@gmail.com>
2024-03-25 15:16:43 +0100

    Adds all selects to form

--- components/user-form/index.tsx
+++ components/user-form/index.tsx
@@ -1,5 +1,4 @@
-import AgeSelect from "./ages-select";
-import GenderSelect from "./gender-select";
+import Select from "./select";
 import SalaryInput from "./salary-input";
 
 export type User = {
@@ -8,12 +7,30 @@ export type User = {
 };
 
 export default function UserForm(props: { user: User; setUser: CallableFunction }) {
-  const setUser = (newUser: User) => props.setUser({ ...props.user, ...newUser });
+  const setUser = (newUser: User) => {
+    console.log({ oldUser: props.user, newUser });
+    props.setUser({ ...props.user, ...newUser });
+  };
   return (
     <form onSubmit={(event) => event.preventDefault()}>
       <SalaryInput setUser={setUser} />
-      <GenderSelect setUser={setUser} />
-      <AgeSelect setUser={setUser} />
+
+      <Select dimension="age" setUser={setUser} />
+      <Select dimension="gender" setUser={setUser} />
+      <Select dimension="citizenship" setUser={setUser} />
+      <Select dimension="companySize" setUser={setUser} />
+      <Select dimension="education" setUser={setUser} />
+      <Select dimension="experience" setUser={setUser} />
+      <Select dimension="bonus" setUser={setUser} />
+      <Select dimension="equity" setUser={setUser} />
+      <Select dimension="position" setUser={setUser} />
+      <Select dimension="organizationType" setUser={setUser} />
+      <Select dimension="industry" setUser={setUser} />
+      <Select dimension="role" setUser={setUser} />
+      <Select dimension="yearsInCurrentCompany" setUser={setUser} />
+      <Select dimension="yearsInCurrentPosition" setUser={setUser} />
+      <Select dimension="salaryRaise" setUser={setUser} />
+      <Select dimension="workingSchedule" setUser={setUser} />
     </form>
   );
 }
