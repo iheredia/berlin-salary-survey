241aa06bc8e2ef0321bc41f6303e3484c6686cee
Ignacio Heredia <ignacio.nh@gmail.com>
2024-03-25 16:35:37 +0100

    More styles for form

--- components/user-form/index.tsx
+++ components/user-form/index.tsx
@@ -15,23 +15,27 @@ export default function UserForm(props: { user: User; setUser: CallableFunction 
   };
   return (
     <form className={styles.form} onSubmit={(event) => event.preventDefault()}>
+      <h3>How do you compare?</h3>
+
       <SalaryInput setUser={setUser} />
 
-      <Select dimension="age" setUser={setUser} />
-      <Select dimension="gender" setUser={setUser} />
-      <Select dimension="experience" setUser={setUser} />
+      <p className={styles.selectContainer}>
+        <Select dimension="age" setUser={setUser} />
+        <Select dimension="gender" setUser={setUser} />
+        <Select dimension="experience" setUser={setUser} />
 
-      <Select dimension="citizenship" setUser={setUser} />
-      <Select dimension="companySize" setUser={setUser} />
-      <Select dimension="education" setUser={setUser} />
-      <Select dimension="position" setUser={setUser} />
-      <Select dimension="organizationType" setUser={setUser} />
-      <Select dimension="industry" setUser={setUser} />
-      <Select dimension="role" setUser={setUser} />
-      <Select dimension="yearsInCurrentCompany" setUser={setUser} />
-      <Select dimension="yearsInCurrentPosition" setUser={setUser} />
-      <Select dimension="salaryRaise" setUser={setUser} />
-      <Select dimension="workingSchedule" setUser={setUser} />
+        <Select dimension="citizenship" setUser={setUser} />
+        <Select dimension="companySize" setUser={setUser} />
+        <Select dimension="education" setUser={setUser} />
+        <Select dimension="position" setUser={setUser} />
+        <Select dimension="organizationType" setUser={setUser} />
+        <Select dimension="industry" setUser={setUser} />
+        <Select dimension="role" setUser={setUser} />
+        <Select dimension="yearsInCurrentCompany" setUser={setUser} />
+        <Select dimension="yearsInCurrentPosition" setUser={setUser} />
+        <Select dimension="salaryRaise" setUser={setUser} />
+        <Select dimension="workingSchedule" setUser={setUser} />
+      </p>
     </form>
   );
 }
--- components/user-form/select.tsx
+++ components/user-form/select.tsx
@@ -4,23 +4,18 @@ import values from "@/data/values";
 export default function Select(props: { setUser: CallableFunction; dimension: StringDimension }) {
   const defaultValue = "Prefer not to say";
   return (
-    <p>
-      <label>
-        {names[props.dimension]}
-        <select
-          defaultValue={defaultValue}
-          onChange={(event) => {
-            props.setUser({ [props.dimension]: event.currentTarget.value });
-          }}
-        >
-          <option value={defaultValue}>{defaultValue}</option>
-          {values[props.dimension].map((value) => (
-            <option key={value} value={value}>
-              {value}
-            </option>
-          ))}
-        </select>
-      </label>
-    </p>
+    <select
+      defaultValue={defaultValue}
+      onChange={(event) => {
+        props.setUser({ [props.dimension]: event.currentTarget.value });
+      }}
+    >
+      <option value={defaultValue}>{names[props.dimension]}</option>
+      {values[props.dimension].map((value) => (
+        <option key={value} value={value}>
+          {names[props.dimension]}: {value}
+        </option>
+      ))}
+    </select>
   );
 }
