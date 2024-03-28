807755b3201c5c55523af1fbc3e6c53169b17587
Ignacio Heredia <ignacio.nh@gmail.com>
2024-03-28 21:26:17 +0100

    Accent color and svg

--- components/introduction/index.tsx
+++ components/introduction/index.tsx
@@ -8,7 +8,20 @@ export default function Introduction(props: { year: AvailableYear }) {
   return (
     <section className={styles.introduction}>
       <div className={classNames(styles.titleContainer, lora.className)}>
-        <h1>Berlin salary survey {year}</h1>
+        <h1>
+          <svg
+            viewBox="0 0 68.98 38.88"
+            y="0px"
+            x="0px"
+            id="Layer_1"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <polygon points="15.51,38.88 0,38.88 22.45,0 37.96,0"></polygon>
+            <polygon points="46.53,38.88 31.02,38.88 53.47,0 68.98,0"></polygon>
+          </svg>
+          Berlin salary survey {year}
+        </h1>
         <h2>How are you paid?</h2>
       </div>
       <p>Welcome to the analysis of the {year} Handpicked Berlin Salary Survey.</p>
