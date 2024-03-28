c0d24778af2311ba544fe024d0fc5673cc319c9b
Ignacio Heredia <ignacio.nh@gmail.com>
2024-03-28 20:37:03 +0100

    Adds lora and montserrat

---
+++ components/fonts.ts
@@ -0,0 +1,6 @@
+import { Lora, Montserrat } from "next/font/google";
+
+const lora = Lora({ subsets: ["latin", "latin-ext"] });
+const montserrat = Montserrat({ subsets: ["latin", "latin-ext"] });
+
+export { montserrat, lora };
--- components/introduction/index.tsx
+++ components/introduction/index.tsx
@@ -1,15 +1,19 @@
 import { AvailableYear } from "@/data/types";
-import styles from "./introduction.module.css";
+import { lora, montserrat } from "../fonts";
+import styles from "./index.module.css";
+import classNames from "classnames";
 
 export default function Introduction(props: { year: AvailableYear }) {
   const { year } = props;
   return (
     <section className={styles.introduction}>
-      <h1>Berlin salary survey {year}</h1>
-      <h2>How are you paid?</h2>
+      <div className={classNames(styles.titleContainer, lora.className)}>
+        <h1>Berlin salary survey {year}</h1>
+        <h2>How are you paid?</h2>
+      </div>
       <p>Welcome to the analysis of the {year} Handpicked Berlin Salary Survey.</p>
       <p>
-        Here you can find general statistics of the survey and an interactive section to see{" "}
+        Here you can find general statistics of the survey with an interactive section to see{" "}
         <strong>how does your salary compare?</strong> against the survey.
       </p>
       <p>
