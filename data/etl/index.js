9d73173562d723be054bffd9a808ae6ded997623
Ignacio Heredia <ignacio.nh@gmail.com>
2024-04-04 21:18:14 +0200

    ETL improvements

--- data/etl/index.js
+++ data/etl/index.js
@@ -1,53 +1,16 @@
 const csv = require("csvtojson");
 const path = require("path");
-const { writeJson, getUniqueValues } = require("./utils");
+const { parseRow, writeJson, getUniqueValues } = require("./utils");
 
 csv({
   delimiter: ",",
-  noheader: true,
 })
   .fromFile(path.join(__dirname, "./2023.csv"))
   .then((rawData) => {
     console.log(`Raw count: ${rawData.length}`);
 
-    const uniqueValues = {
-      age: new Set(),
-      gender: new Set(),
-      education: new Set(),
-    };
-
     const cleanData = rawData
-      .map((row) => {
-        return {
-          inBerlin: row.field2 === "Yes, I'm physically working in Berlin.",
-          workingRemotelyForBerlin:
-            row.field2 === "Yes, I'm working remotely for a Berlin-based organization.",
-
-          gender: row.field3,
-          age: row.field4,
-          education: row.field5 === "na" ? "Prefer not to say" : row.field5,
-          experience: row.field6 === "na" ? "Prefer not to say" : row.field6,
-          citizenship: row.field7 === "na" ? "Prefer not to say" : row.field7,
-          organizationType: row.field8,
-          industry: row.field9,
-          companySize: row.field10 === "" ? "Prefer not to say" : row.field10,
-          yearsInCurrentCompany: row.field11 === "na" ? "Prefer not to say" : row.field11,
-          position: row.field12,
-          role: row.field13 === "na" ? "Prefer not to say" : row.field13,
-          yearsInCurrentPosition: row.field14 === "na" ? "Prefer not to say" : row.field14,
-
-          workingSchedule: row.field15 === "na" ? "Prefer not to say" : row.field15,
-          isFullTime: row.field15 === "Full-time",
-          isPartTime: row.field15 === "Part-time",
-          isFreelance: row.field15 === "Contract/Freelance",
-
-          hoursPerWeek: parseFloat(row.field16.replace(",", ".")),
-          grossSalary: parseInt(row.field17.replace(/\D/g, "")),
-          bonus: parseInt(row.field18.replace(/\D/g, "")) || 0,
-          equity: parseInt(row.field19.replace(/\D/g, "")) || 0,
-          salaryRaise: row.field20 === "na" ? "Prefer not to say" : row.field20,
-        };
-      })
+      .map(parseRow)
       .filter((row) => row.inBerlin || row.workingRemotelyForBerlin)
       .filter((row) => !isNaN(row.grossSalary));
 
--- data/etl/utils.js
+++ data/etl/utils.js
@@ -2,18 +2,61 @@ const fs = require("fs");
 const path = require("path");
 const stringify = require("json-stable-stringify");
 
+const ignoreKeysForUniqueValues = ["bonus", "equity", "grossSalary", "hoursPerWeek"];
+
+function sanitizeString(val) {
+  if (val === "na" || val === "") return "Prefer not to say";
+  return val;
+}
+
+function sanitizeInt(stringVal) {
+  return parseInt(stringVal.replace(/\D/g, "")) || 0;
+}
+
+function parseRow(row) {
+  const parsedRow = {
+    inBerlin: row.inBerlin === "Yes, I'm physically working in Berlin.",
+    workingRemotelyForBerlin:
+      row.inBerlin === "Yes, I'm working remotely for a Berlin-based organization.",
+
+    gender: sanitizeString(row.gender),
+    age: sanitizeString(row.age),
+    education: sanitizeString(row.education),
+    experience: sanitizeString(row.experience),
+    citizenship: sanitizeString(row.citizenship),
+    organizationType: sanitizeString(row.organizationType),
+    industry: sanitizeString(row.industry),
+    companySize: sanitizeString(row.companySize),
+    yearsInCurrentCompany: sanitizeString(row.yearsInCurrentCompany),
+    position: sanitizeString(row.position),
+    role: sanitizeString(row.role),
+    yearsInCurrentPosition: sanitizeString(row.yearsInCurrentPosition),
+
+    workingSchedule: sanitizeString(row.workingSchedule),
+    isFullTime: row.workingSchedule === "Full-time",
+    isPartTime: row.workingSchedule === "Part-time",
+    isFreelance: row.workingSchedule === "Contract/Freelance",
+
+    hoursPerWeek: parseFloat(row.hoursPerWeek.replace(",", ".")),
+    grossSalary: sanitizeInt(row.grossSalary),
+    bonus: sanitizeInt(row.bonus),
+    equity: sanitizeInt(row.equity),
+    salaryRaise: sanitizeString(row.salaryRaise),
+  };
+
+  return parsedRow;
+}
+
 function writeJson(dataObj, filePath) {
   const dataObjString = stringify(dataObj, { space: "  " });
   fs.writeFileSync(path.join(__dirname, filePath), dataObjString);
 }
 
 function getUniqueValues(data) {
-  const ignoreKeys = ["bonus", "equity", "grossSalary", "hoursPerWeek"];
-
   const uniqueValues = {};
   data.forEach((row) =>
     Object.keys(row).forEach((key) => {
-      if (ignoreKeys.includes(key)) return;
+      if (ignoreKeysForUniqueValues.includes(key)) return;
       if (!uniqueValues[key]) {
         uniqueValues[key] = new Set();
       }
@@ -28,4 +71,4 @@ function getUniqueValues(data) {
   return uniqueValues;
 }
 
-module.exports = { writeJson, getUniqueValues };
+module.exports = { parseRow, writeJson, getUniqueValues };
