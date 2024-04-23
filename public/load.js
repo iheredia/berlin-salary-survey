14032476f9630be85f3e30f7b63f51572e5a9712
Ignacio Heredia <ignacio.nh@gmail.com>
2024-04-23 17:56:32 +0200

    Adds iframe resizer

---
+++ public/load.js
@@ -0,0 +1,30 @@
+(function () {
+  var thisScript = document.querySelector("#salaries-dashboard-script");
+  function insertAfter(existingElement, newElement) {
+    existingElement.parentNode.insertBefore(newElement, existingElement.nextSibling);
+  }
+
+  var css = "#salaries-iframe { width: 1px; min-width: 100%; }";
+  var styleElement = document.createElement("style");
+  styleElement.type = "text/css";
+  if (styleElement.styleSheet) {
+    // This is required for IE8 and below.
+    styleElement.styleSheet.cssText = css;
+  } else {
+    styleElement.appendChild(document.createTextNode(css));
+  }
+  insertAfter(thisScript, styleElement);
+
+  const iframeResizerScript = document.createElement("script");
+  iframeResizerScript.type = "text/javascript";
+  iframeResizerScript.src = "https://berlin-salary-survey.iheredia.com/iframeResizer.min.js";
+  insertAfter(styleElement, iframeResizerScript);
+
+  const dashboardIframe = document.createElement("iframe");
+  dashboardIframe.id = "dashboard-iframe";
+  dashboardIframe.src = "https://berlin-salary-survey.iheredia.com/2024?embed=true";
+  dashboardIframe.frameborder = "0";
+  insertAfter(iframeResizerScript, dashboardIframe);
+
+  window.iFrameResize({ log: true }, "#dashboard-iframe");
+})();
